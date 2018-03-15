d3304d4fa8c0c773e2135e331defceefdf430c1e
hpierre58 <37367714+hpierre58@users.noreply.github.com>
2018-03-15 14:55:19 +0100

    Page plus dynamique

--- js/sb-admin-charts.js
+++ js/sb-admin-charts.js
@@ -3,12 +3,41 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
+function timestampToMyString(myTimestamp){
+
+  var hours = new Date(myTimestamp).getHours();
+  var minutes = new Date(myTimestamp).getMinutes();
+
+  hours = (hours<10) ? '0' + hours : hours;
+  minutes = (minutes<10) ? '0' + minutes : minutes;
+
+  var myString = hours + ':' + minutes;
+
+  return myString;
+}
+
+// Exemples datas
+var date1 = timestampToMyString(1382086394000);
+var date2 = timestampToMyString(1382096395000);
+var date3 = timestampToMyString(1382186396000);
+var date4 = timestampToMyString(1382286397000);
+var date5 = timestampToMyString(1382386398000);
+
+var TempData1 = 36;
+var TempData2 = 37;
+var TempData3 = 35;
+var TempData4 = 40;
+var TempData5 = 43;
+
 // -- Area Chart 1
 var ctx = document.getElementById("myAreaChart");
+var divLastTemp = document.getElementById("lastTemp");  
+divLastTemp.textContent = TempData5;  
+var text = divLastTemp.textContent;  
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+    labels: [date1, date2, date3, date4, date5],
     datasets: [{
       label: "Sessions",
       lineTension: 0.3,
@@ -21,7 +50,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBackgroundColor: "rgba(0,165,0,1)",
       pointHitRadius: 20,
       pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+      data: [TempData1, TempData2, TempData3, TempData4, TempData5],
     }],
   },
   options: {
@@ -40,7 +69,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 40000,
+          max: 50,
           maxTicksLimit: 5
         },
         gridLines: {
