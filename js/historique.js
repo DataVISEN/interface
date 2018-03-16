d4d3d542fe6ee58f5d5b9e1448a772e443cf0ce6
Adrien Philippeau <adrien.philippeau@orange.fr>
2018-03-16 10:21:35 +0100

    DEV
    historique.js

--- js/historique.js
+++ js/historique.js
@@ -9,7 +9,9 @@ function getDate(){
 	var date = document.getElementById('datepicker').value;
 	console.log(date);
 	date=date.split("/");
-	var newDate=date[1]+","+date[0]+","+date[2];
-	console.log(date[1]+","+date[0]+","+date[2]);
-	alert(new Date(newDate).getTime());​ //will alert 1330192800000
+	var newDate=date[1]+"/"+date[0]+"/"+date[2];
+	console.log(newDate);
+	var date = new Date(newDate)
+	var now = Date.now();
+	return {from:truc.getTime(), to:now};
 }
